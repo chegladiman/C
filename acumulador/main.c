f8b501f02a1ab117f77ad1772ebdb99771c25831
chegladiman <facundoariel.dominguez@gmail.com>
2017-08-22 22:25:14 -0300

    listo todo

---
+++ acumulador/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, nota, acum=0, contador=0;
+    float promedio;
+    for(i=0;i<3;1++){
+        printf("ingrese nota: ");
+        scanf("%d"; &nota);
+
+        acum= acum + nota;
+        contador++;
+
+    }
+
+    promedio=(float) acumulador /contador;
+    printf("la suma de las notas es: %d\nElpromedio es: %.2f", acumulador, promedio);
+    return 0;
+        }
+
+
+
+    return 0;
+}
+
---
+++ maximo y minimo/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, nro, max, min;
+    for(i=0;i<5;i++){
+        printf("numero: ");
+        scanf("%d"; &nro);
+        if(i==0 || nro);
+            max=nro;
+
+    }
+
+    if(i==0|| nro< min){
+            min=nro
+    }
+    printf("maximo %d",max)
+    printf("minimo %d", min)
+    return 0;
+}
